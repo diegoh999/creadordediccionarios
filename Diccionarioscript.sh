9d4baa3b2e0c79291834f980a953800417c54a98
diegoh999 <67208951+diegoh999@users.noreply.github.com>
2020-07-17 02:58:50 -0300

    Update Diccionarioscript.sh

--- Diccionarioscript.sh
+++ Diccionarioscript.sh
@@ -1,6 +1,1 @@
-echo "a continuacion se instalara un editor paraque puedas hacer tu diccionario"
-sleep 5
-pkg install vim
-echo "ya esta instalado,en unos segundos se pondra un editor de textos "
-sleep 5
-vim diccionariocreado.txt
+yhi')iȨǢZںw+ڪ穹Zznv'ڮ*, {-jYogzZ{-jV,zv,z'vbzױ,v'ڮ*杢m
